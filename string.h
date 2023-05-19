e887db488a4490048ce40e0e8d8b27fee425d8b9
yousef <yabdelshafy0@gmail.com>
2023-05-19 16:27:37 +0300

    add string.h header file

---
+++ string.h
@@ -0,0 +1,12 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdlib.h>
+#include <string.h>
+
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int my_strncmp(const char *str1, const char *str2, size_t n);
+char *_strdup(char *str);
+
+#endif
