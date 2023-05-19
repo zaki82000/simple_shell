6fc2f789f6f9f0aabd1371edcd7e24327e32dd58
yousef <yabdelshafy0@gmail.com>
2023-05-19 11:51:29 +0300

    add tokens.h header file

---
+++ tokens.h
@@ -0,0 +1,25 @@
+#ifndef TOKENS_H
+#define	TOKENS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* struct node - The given code snippet defines a structure called node
+* @data: This member is a pointer to a character
+* @next: This member is a pointer to another token_t structure
+*/
+
+typedef struct node
+{
+	char *data;
+	struct node *next;
+} token_t;
+
+token_t *add_token(token_t *head, char *token);
+token_t *create_tokens(char *line);
+size_t count_tokens(token_t *head);
+void free_tokens(token_t *head);
+
+#endif
